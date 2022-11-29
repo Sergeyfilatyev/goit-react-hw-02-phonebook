de1b11e238a0cc5270c2ab1a00777dcba5f9e8d9
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-11-29 10:58:10 +0200

    --

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,16 @@
 import s from './Filter.module.css';
 export function Filter({ value, onChange }) {
   return (
-    <input
-      className={s.input}
-      value={value}
-      onChange={onChange}
-      type="text"
-      name="filter"
-      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-    ></input>
+    <label className={s.label}>
+      Find contacts by name
+      <input
+        className={s.input}
+        value={value}
+        onChange={onChange}
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      ></input>
+    </label>
   );
 }
